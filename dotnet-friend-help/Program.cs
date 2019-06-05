661085e79572933718cf57375d0ed36df634e334
Diego Luiz <diegorcl@gmail.com>
2019-06-05 23:11:53 +0100

    refactor

--- dotnet-friend-help/Program.cs
+++ dotnet-friend-help/Program.cs
@@ -21,7 +21,7 @@ namespace dotnet_friend_help
     {
         public override bool IsSimple => false;
 
-        public List<Tuple<BasePart, int>> Parts { get; set; } = new List<Tuple<BasePart, int>>();
+        public List<Tuple<BasePart, int>> SubParts { get; set; } = new List<Tuple<BasePart, int>>();
     }
 
     class Program
@@ -29,44 +29,63 @@ namespace dotnet_friend_help
         static void Main(string[] args)
         {
             var file = @"./input.txt";
+            var parts = ReadFile(file);
 
-            var partsList = new Dictionary<string, BasePart>();
+            foreach (var part in parts)
+            {
+                Console.WriteLine($"{part.Value.Name} - {part.Value.IsSimple} - {part.Value.Value}");
+            }
+
+        }
+
+        private static Dictionary<string, BasePart> ReadFile(string file)
+        {
+            var parts = new Dictionary<string, BasePart>();
 
             using (var fileStream = new StreamReader(file))
             {
-                string line;
-                line = fileStream.ReadLine();
-                var simplePartCount = int.Parse(line);
+                LoadSimpleParts(parts, fileStream);
+                LoadCompound(parts, fileStream);
+            }
+            return parts;
+        }
 
-                for (var i = 0; i < simplePartCount; i++)
+        private static void LoadCompound(Dictionary<string, BasePart> parts, StreamReader fileStream)
+        {
+            string line;
+            while ((line = fileStream.ReadLine()) != null)
+            {
+                var lineInfo = line.Split(" ");
+                var name = lineInfo[0];
+                var subPartName = lineInfo[1];
+                var subPartCount = int.Parse(lineInfo[2]);
+
+                if (!parts.ContainsKey(name))
                 {
-                    var simplePartInfo = fileStream.ReadLine().Split(" ");
-                    var simplePart = new SimplePart
-                    {
-                        Name = simplePartInfo[0],
-                        Value = int.Parse(simplePartInfo[1])
-                    };
-                    partsList.Add(simplePart.Name, simplePart);
+                    parts.Add(name, new CompoundPart { Name = name });
                 }
 
-                while ((line = fileStream.ReadLine()) != null)
-                {
-                    var compoundPartInfo = line.Split(" ");
-                    var name = compoundPartInfo[0];
-                    var simplePartName = compoundPartInfo[1];
-                    var count = int.Parse(compoundPartInfo[2]);
-
-                    if (!partsList.ContainsKey(name))
-                    {
-                        partsList.Add(name, new CompoundPart { Name = name });
-                    }
-
-
-                    var compoundPart = (CompoundPart)partsList[name];
-                    compoundPart.Parts.Add(new Tuple<BasePart, int>(partsList[simplePartName], count));
-                }
+                var part = (CompoundPart)parts[name];
+                var subPart = parts[subPartName];
+                part.SubParts.Add(new Tuple<BasePart, int>(subPart, subPartCount));
             }
         }
 
+        private static void LoadSimpleParts(Dictionary<string, BasePart> parts, StreamReader fileStream)
+        {
+            string line = fileStream.ReadLine();
+            var simplePartCount = int.Parse(line);
+
+            for (var i = 0; i < simplePartCount; i++)
+            {
+                var simplePartInfo = fileStream.ReadLine().Split(" ");
+                var simplePart = new SimplePart
+                {
+                    Name = simplePartInfo[0],
+                    Value = int.Parse(simplePartInfo[1])
+                };
+                parts.Add(simplePart.Name, simplePart);
+            }
+        }
     }
 }
